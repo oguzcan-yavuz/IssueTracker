2ada1c2571ce28ffd7fcdbf4048073d023f407d0
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-08-23 21:40:47 +0300

    some little improvements

--- charts/urls.py
+++ charts/urls.py
@@ -4,7 +4,7 @@ from .views import *
 # charts/
 urlpatterns = [
     url(r'^$', ChartView.as_view(), name="chart_main"),
-    url(r'^issue_chart/$', IssueChartView.as_view(), name="issue_chart"),
+    url(r'^issue_charts/$', IssueChartView.as_view(), name="issue_chart"),
     # make rest endpoints global in website, not under charts app
     url(r'^rest/$', RestApiView.as_view(), name="rest"),
     url(r'^rest/chart/$', ChartApiView.as_view(), name="chart_api"),
