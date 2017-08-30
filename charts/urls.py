bcdae5785e26dc9b42a0c5426e5291abae9ac854
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-08-30 13:41:15 +0300

    TechGuyIssueView created.

--- charts/urls.py
+++ charts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^category_issue/$', CategoryIssueView.as_view(), name="category_issue_chart"),
     url(r'^customer_issue/$', CustomerIssueView.as_view(), name="customer_issue_chart"),
     url(r'^product_issue/$', ProductIssueView.as_view(), name="product_issue_chart"),
+    url(r'^tech_guy_issue/$', TechGuyIssueView.as_view(), name="tech_guy_issue_chart"),
     # StatisticsView url
     url(r'^statistics/$', StatisticView.as_view(), name="profit_json"),
 ]
--- charts/views.py
+++ charts/views.py
@@ -34,6 +34,10 @@ class ProductIssueView(LoginRequiredMixin, TemplateView):
     template_name = 'charts/product_issue.html'
 
 
+class TechGuyIssueView(LoginRequiredMixin, TemplateView):
+    template_name = 'charts/tech_guy_issue.html'
+
+
 # returns all data for each model in issues.models
 
 class RestApiView(LoginRequiredMixin, APIView):
@@ -90,68 +94,68 @@ class StatisticView(LoginRequiredMixin, View):
     def get(self, request):
         if request.is_ajax():
             statistic = int(request.GET.get('statistic'))
-            first_date = request.GET.get('first_date')
-            last_date = request.GET.get('last_date')
-            first_date = datetime.strptime(first_date, "%Y-%m-%dT%H:%M:%S.%fZ")
-            last_date = datetime.strptime(last_date, "%Y-%m-%dT%H:%M:%S.%fZ")
-            first_date = timezone.make_aware(first_date, timezone.get_current_timezone())
-            last_date = timezone.make_aware(last_date, timezone.get_current_timezone())
-            day = timedelta(days=1)
+            if statistic != 5:
+                first_date = request.GET.get('first_date')
+                last_date = request.GET.get('last_date')
+                first_date = datetime.strptime(first_date, "%Y-%m-%dT%H:%M:%S.%fZ")
+                last_date = datetime.strptime(last_date, "%Y-%m-%dT%H:%M:%S.%fZ")
+                first_date = timezone.make_aware(first_date, timezone.get_current_timezone())
+                last_date = timezone.make_aware(last_date, timezone.get_current_timezone())
+                day = timedelta(days=1)
+                if statistic == 1:
+                    """This statistic gives issues done in given date gaps."""
+                    data = Issue.objects.filter(delivery_time__gte=first_date,
+                                                delivery_time__lte=last_date, status='DO')
+                    return JsonResponse(serializers.serialize('json', data), safe=False)
+                elif statistic == 2:
+                    """This statistic gives the issues's count which grouped by categories
+                    in given date gap."""
+                    data = Issue.objects.filter(
+                        creation_time__gte=first_date, creation_time__lte=last_date).values(
+                        'product__category__name').annotate(count=Count('product__category'))
 
-            if statistic == 1:
-                """This statistic gives issues done in given date gaps."""
-                data = Issue.objects.filter(delivery_time__gte=first_date,
-                                            delivery_time__lte=last_date, status='DO')
-                return JsonResponse(serializers.serialize('json', data), safe=False)
-            elif statistic == 2:
-                """This statistic gives the issues's count which grouped by categories
-                in given date gap."""
-                data = Issue.objects.filter(
-                    creation_time__gte=first_date, creation_time__lte=last_date).values(
-                    'product__category__name').annotate(count=Count('product__category'))
+                    # We are doing serialization this way when our queryset is ValuesQuerySet.
+                    # Because regular django serializers can't serialize it.
+                    data = json.dumps(list(data), cls=DjangoJSONEncoder)
+                    return JsonResponse(data, safe=False)
+                elif statistic == 3:
+                    """This statistic gives the customers's count which created issue in given
+                    date gap."""
+                    data = []
+                    current_date = first_date
 
-                # We are doing serialization this way when our queryset is ValuesQuerySet.
-                # Because regular django serializers can't serialize it.
-                data = json.dumps(list(data), cls=DjangoJSONEncoder)
-                return JsonResponse(data, safe=False)
-            elif statistic == 3:
-                """This statistic gives the customers's count which created issue in given
-                date gap."""
-                data = []
-                current_date = first_date
+                    while current_date <= last_date:
+                        current_date_range = current_date + day
+                        data.append([{'date': current_date}] + list(Issue.objects.filter(
+                            creation_time__range=(current_date, current_date_range)).values(
+                            'customer__name').annotate(
+                            customer_count=Count('customer'))))
+                        current_date += day
 
-                while current_date <= last_date:
-                    current_date_range = current_date + day
-                    data.append([{'date': current_date}] + list(Issue.objects.filter(
-                        creation_time__range=(current_date, current_date_range)).values(
-                        'customer__name').annotate(
-                        customer_count=Count('customer'))))
-                    current_date += day
+                    data = json.dumps(data, cls=DjangoJSONEncoder)
+                    return JsonResponse(data, safe=False)
+                elif statistic == 4:
+                    """This statistic gives the products's count which have an issue in given
+                    date gap."""
+                    data = []
+                    current_date = first_date
 
-                data = json.dumps(data, cls=DjangoJSONEncoder)
-                return JsonResponse(data, safe=False)
-            elif statistic == 4:
-                """This statistic gives the products's count which have an issue in given
-                date gap."""
-                data = []
-                current_date = first_date
+                    while current_date <= last_date:
+                        current_date_range = current_date + day
+                        data.append([{'date': current_date}] + list(Issue.objects.filter(
+                            creation_time__range=(current_date, current_date_range)).values(
+                            'product__name').annotate(product_count=Count('product'))))
+                        current_date += day
 
-                while current_date <= last_date:
-                    current_date_range = current_date + day
-                    data.append([{'date': current_date}] + list(Issue.objects.filter(
-                        creation_time__range=(current_date, current_date_range)).values(
-                        'product__name').annotate(product_count=Count('product'))))
-                    current_date += day
-
-                data = json.dumps(data, cls=DjangoJSONEncoder)
-                return JsonResponse(data, safe=False)
-            elif statistic == 5:
+                    data = json.dumps(data, cls=DjangoJSONEncoder)
+                    return JsonResponse(data, safe=False)
+            else:
                 """This statistic gives the average time of problem solve of tech guys
                 and count of problems they have solved."""
-                data = Issue.objects.filter(status='DO').values('tech_guy').annotate(
+                data = Issue.objects.filter(status='DO').values('tech_guy__username').annotate(
                     fix_time_avg=Avg(ExpressionWrapper(F('delivery_time') - F('creation_time'),
                                                        output_field=DurationField())),
-                    solved_issue_count=Count('tech_guy')).order_by('ort')
+                    solved_issue_count=Count('tech_guy')).order_by('fix_time_avg')
                 data = json.dumps(list(data), cls=DjangoJSONEncoder)
                 return JsonResponse(data, safe=False)
         else:
